403f1e289ad854919ca57babf1af0f95246d5454
Volodymyr Bodnarchuk <got'volod@DESKTOP-183OQ7B.(none)')>
2023-03-20 16:23:38 +0200

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Computer comp = new Computer("IBM XT", 80000);
-        SaveInterface saver = new SaveComputerToFile();
+        SaveInterface saver = new SaveComputerToDB();
         saver.save("out.dat", comp);
     }
 }
